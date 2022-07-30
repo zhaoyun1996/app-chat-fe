974a12d70c41aaaf62700e460d0f84c031472e17
zhaoyun1996 <kiet14020248uet@gmail.com>
2022-07-30 15:56:15 +0700

    Thêm config

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueSocketIO from 'vue-socket.io'
+import Vue from "vue";
+import App from "./App.vue";
+import VueSocketIO from "vue-socket.io";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(new VueSocketIO({
-  debug: true,
-  connection: 'http://localhost:3000',
-}))
+Vue.use(
+    new VueSocketIO({
+        debug: true,
+        connection: `${import.meta.env.VITE_API_URL}`,
+    })
+);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: (h) => h(App),
+}).$mount("#app");
